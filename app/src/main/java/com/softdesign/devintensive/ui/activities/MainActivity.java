28172a2821fe35adf1b44370ea3a137d9c949ad7
Alexander <Vdovenkov>
2016-06-26 22:28:11 +0300

    Добавил базовый класс BaseActivity Добавил методы в BaseActivity Добавил координатор в макет активности. Добавил AppBar, NavigationDrawer и ToolBar, изменил их компановку и прозрачность. И ещё много различных мелких изменений и дополнений по видео. Прогресс 1:08:02

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,74 +1,101 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
 
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            Log.d(TAG, "onCreate");
 
-            if (savedInstanceState == null) {
-                //активность запускается впервые
-            } else {
-                //активность уже запускалась
-            }
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
 
-        @Override
-        protected void onStart() {
-            super.onStart();
-            Log.d(TAG, "onStart");
-        }
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        @Override
-        protected void onResume() {
-            super.onResume();
-            Log.d(TAG, "onResume");
-        }
+        setupToolBar();
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            Log.d(TAG, "onPause");
-        }
-
-        @Override
-        protected void onStop() {
-            super.onStop();
-            Log.d(TAG, "onStop");
-        }
-
-        @Override
-        protected void onRestart() {
-            super.onRestart();
-            Log.d(TAG, "onRestart");
-        }
-
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            Log.d(TAG, "onDestroy");
-        }
-
-        @Override
-        protected void onSaveInstanceState(Bundle outState) {
-            super.onSaveInstanceState(outState);
-            Log.d(TAG, "onSaveInstanceState");
-        }
-
-        @Override
-        public void onClick(View v) {
-            Log.d(TAG, "onClick");
+        if (savedInstanceState == null) {
+            //активность запускается впервые
+        } else {
+            //активность уже запускалась
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick");
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolBar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+}
+
+
